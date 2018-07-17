6f04e9fa6f0aca53aa4ac86ed45f7340da6dc599
zcochran <fuzzylimes@gmail.com>
2018-07-17 16:57:28 -0400

    Summary section display
    Top section will now display the combination of stats across all running instances.

--- public/js/client.js
+++ public/js/client.js
@@ -1,8 +1,10 @@
+let summary = {};
+let total = { tx: 0, rx: 0, success: 0, codes: {} };
+let responseCodes = [];
+
 var socket = io('http://localhost:5555');
 socket.on('data update', function (data) {
     // console.log(data);
-    let summary = {};
-    let total = {tx: 0, rx: 0, success: 0, codes: {}};
     data.forEach(function (o) {
         var server = Object.keys(o)[0];
         setStatus(server, o);
@@ -14,6 +16,7 @@ socket.on('data update', function (data) {
         let urls = Object.keys(o[server].urls);
         let responses = getCodes(o[server]);
         responses = [...new Set(responses)].sort();
+        responseCodes = [...new Set(responseCodes.concat([...responses]))].sort();
         let myHtml = "";
         urls.forEach(url => {
             let p =  o[server].urls[url];
@@ -23,6 +26,7 @@ socket.on('data update', function (data) {
                 total.tx += p[m].tx;
                 total.rx += p[m].rx;
                 total.success += p[m].expected;
+                addToSummary(url, p[m]);
                 let c;
                 success < 100 ? c = 'class="table-warning"' : c = 'class="table-success"';
                 myHtml += `<tr ${c}><td>${url}</td><td>${m}</td><td>${p[m].tx}</td><td>${p[m].rx}</td>`
@@ -31,7 +35,7 @@ socket.on('data update', function (data) {
                     myHtml += `<td>${p[m].responses[r]}</td>` :
                     myHtml += `<td>0</td>`;
                 });
-                success < 100 ? success = succes.toFixed(4) : true; 
+                success < 100 ? success = success.toFixed(4) : true; 
                 myHtml += `<td>${p[m].times.min.toFixed(4)}</td><td>${p[m].times.max.toFixed(4)}</td><td>${(p[m].times.sum/p[m].tx).toFixed(4)}</td>
                 <td>${success}%</td></tr>`
             });
@@ -47,12 +51,53 @@ socket.on('data update', function (data) {
         card.innerHTML = head;
 
     });
+    buildTopSummary();
+});
+
+function addToSummary(url, data){
+    if(!summary.hasOwnProperty(url)){
+        summary[url] = {tx:0, rx:0, success:0, codes: {}};
+    }
+    summary[url].tx = data.tx;
+    summary[url].rx = data.rx;
+    summary[url].success = data.expected;
+    responseCodes.forEach(r => {
+        data.responses.hasOwnProperty(r) ? 
+            summary[url].codes[r] = data.responses[r] :
+            summary[url].codes[r] = 0;
+    });
+}
+
+function buildTopSummary(){
+    let myHtml = "";
+    let headers = ['url', 'tx', 'rx'].concat(responseCodes, ['success %']);
+    let head = `<table id="summary-table" class="table"><tr>`;
+    headers.forEach(h => {
+        head += `<th>${h}</th>`
+    });
+    head += `</tr>`;
+    Object.keys(summary).sort().forEach(s => {
+        let success = setSuccess(summary[s].rx, summary[s].success);
+        myHtml += `<tr><td>${s}</td><td>${summary[s].tx}</td><td>${summary[s].rx}</td>`;
+        responseCodes.forEach(r => {
+            myHtml += `<td>${summary[s].codes[r]}</td>`;
+        });
+        myHtml += `<td>${success}%</td></tr>`;
+    });
+    myHtml += `</table>`;
+    document.getElementById('totals-list').innerHTML = head + myHtml;
+
     document.getElementById('tx-total').textContent = total.tx;
     document.getElementById('rx-total').textContent = total.rx;
-    let success = (total.rx / total.success)*100;
-    success < 100 ? success = success.toFixed(4) : true;
+    let success = setSuccess(total.rx, total.success);
     document.getElementById('success-total').textContent = `${success}%`;
-});
+}
+
+function setSuccess(rx, success){
+    success = (success/rx) * 100;
+    success < 100 ? success = success.toFixed(4) : true;
+    return success;
+}
 
 function setStatus(server, o){
     var state = o[server].state;
